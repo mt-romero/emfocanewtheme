76d180a777ddf5c3ed294a9dca7fd16d34ce3814
kpokj <64260842+kpokj@users.noreply.github.com>
2021-09-13 22:31:56 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,12 +1,13 @@
 var toggle = document.querySelector("#toggle-nav")
 var collapse = document.querySelector("#nav-collapse");
 var collapseClasses = collapse.classList;
+var toggleVisible = toggle.classList.contains("show-mobile");
+var isHidden = collapseClasses.contains("hide-mobile");
 
 
 function toggleNavbar() {
     preventDefault();
-    let toggleVisible = toggle.classList.contains("show-mobile");
-    let isHidden = collapseClasses.contains("hide-mobile");
+  
     if (toggleVisible === true) {
         switch (isHidden) {
             case true:
